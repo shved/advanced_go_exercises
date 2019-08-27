d484938196d3bb94f7ae0ffdefd5a0b78cd80028
Vitaly Shvedchenko <vshvedchenko@spbtv.com>
2019-08-27 13:42:26 +0300

    simple process memory and cpu monitor

---
+++ process_monitor/go.mod
@@ -0,0 +1,11 @@
+module .
+
+go 1.12
+
+require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/marcusolsson/tui-go v0.4.0 // indirect
+	github.com/mitchellh/go-ps v0.0.0-20190716172923-621e5597135b // indirect
+	github.com/shirou/gopsutil v2.18.12+incompatible // indirect
+	golang.org/x/sys v0.0.0-20190826190057-c7b8b68b1456 // indirect
+)
---
+++ process_monitor/process_monitor.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	humanize "github.com/dustin/go-humanize"
+	ps "github.com/shirou/gopsutil/process"
+)
+
+var pid int32
+var process ps.Process
+var statusString string
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("no pid provided")
+	}
+
+	pid, err := strconv.ParseInt(os.Args[1], 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	process, err := ps.NewProcess(int32(pid))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		mem, err := process.MemoryInfo()
+		times, err := process.Times()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rss := humanize.Ftoa(float64(mem.RSS / 1024.0 / 1024.0))
+		vms := humanize.Ftoa(float64(mem.VMS / 1024.0 / 1024.0))
+
+		fmt.Printf("\rRSS: %vMB\tVMS: %vMB\tSYS: %v\tUSR: %v", rss, vms, times.System, times.User)
+		time.Sleep(time.Second)
+		fmt.Printf("\r%50v", ' ') // clear status string
+	}
+}
